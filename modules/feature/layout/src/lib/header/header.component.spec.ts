56b5eff724c3506d4eaa48a6d370b35ec8175caf
Willian Grillo <williangrillo@outlook.com.br>
2024-01-22 23:23:48 -0300

    feat(layout): add header

--- modules/feature/layout/src/lib/header/header.component.spec.ts
+++ modules/feature/layout/src/lib/header/header.component.spec.ts
@@ -5,17 +5,24 @@ describe('HeaderComponent', () => {
   let component: HeaderComponent;
   let fixture: ComponentFixture<HeaderComponent>;
 
+  //beforeEach: para cada bloco de teste vai usar essas configurações, execudado antes dos teste
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [HeaderComponent],
     }).compileComponents();
 
-    fixture = TestBed.createComponent(HeaderComponent);
+    fixture = TestBed.createComponent(HeaderComponent); //fixture: ferramentas dom
     component = fixture.componentInstance;
-    fixture.detectChanges();
+    fixture.detectChanges(); //detectar mudancas
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should contain title', () => {
+    const header: HTMLHeadingElement =
+      fixture.nativeElement.querySelector('header');
+    expect(header.textContent).toBe('Ecommerce');
   });
 });
