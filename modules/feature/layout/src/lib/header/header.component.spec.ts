ff2facb42bef7e6b06415a09956fb5d202aa82b4
willian.grillo <williangrillo@outlook.com.br>
2024-01-26 17:22:24 -0500

    feat(layout): add href to logo

--- modules/feature/layout/src/lib/header/header.component.spec.ts
+++ modules/feature/layout/src/lib/header/header.component.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HeaderComponent } from './header.component';
-
+import { RouterTestingModule } from '@angular/router/testing';
 const mockTitle = 'Any Title';
 describe('HeaderComponent', () => {
   let component: HeaderComponent;
@@ -9,6 +9,7 @@ describe('HeaderComponent', () => {
   //beforeEach: para cada bloco de teste vai usar essas configurações, execudado antes dos teste
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [RouterTestingModule],
       declarations: [HeaderComponent],
     }).compileComponents();
 
@@ -30,5 +31,10 @@ describe('HeaderComponent', () => {
     component.title = 'Other title';
     fixture.detectChanges();
     expect(header.textContent).toBe('Other title');
+  });
+
+  it('should redirect to "/" when logo is is clicked', () => {
+    const anchor: HTMLAnchorElement = fixture.nativeElement.querySelector('a');
+    expect(anchor.getAttribute('href')).toBe('/');
   });
 });
