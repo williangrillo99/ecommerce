e22a443120250c3035c3a70698a876a80f1c1264
willian.grillo <williangrillo@outlook.com.br>
2024-02-01 18:02:54 -0300

    test:(product-detail) add tests to quantity description pipe

--- modules/feature/product/detail/src/lib/pipes/quantity-description/quantity-description.pipe.spec.ts
+++ modules/feature/product/detail/src/lib/pipes/quantity-description/quantity-description.pipe.spec.ts
@@ -5,4 +5,21 @@ describe('QuantityDescriptionPipe', () => {
     const pipe = new QuantityDescriptionPipe();
     expect(pipe).toBeTruthy();
   });
+  it('should return unavailable product text', () => {
+    const pipe = new QuantityDescriptionPipe();
+    const description = pipe.transform(0);
+    expect(description).toBe('Produto indisponível');
+  });
+  it('should return text from only one product', () => {
+    const pipe = new QuantityDescriptionPipe();
+    const description = pipe.transform(1);
+    expect(description).toBe(
+      'Útimo produto em estoque. Corra antes que esgote!'
+    );
+  });
+  it('should return text from many products', () => {
+    const pipe = new QuantityDescriptionPipe();
+    const description = pipe.transform(4);
+    expect(description).toBe('4 disponíveis');
+  });
 });
