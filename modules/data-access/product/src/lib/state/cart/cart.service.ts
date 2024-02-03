3a13c814bd2346b7f065a065ca74ac54ee72e748
Willian Grillo <williangrillo@outlook.com.br>
2024-02-03 15:40:04 -0300

    feat(product): add cart

--- modules/data-access/product/src/lib/state/cart/cart.service.ts
+++ modules/data-access/product/src/lib/state/cart/cart.service.ts
@@ -1,8 +1,19 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, map } from 'rxjs';
+import { Product } from '../../models/product.model';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  constructor() {}
+  private cartSubject = new BehaviorSubject<Product[]>([]);
+  cart$ = this.cartSubject.asObservable();
+  // BehaviorSubject x Subject = Diferença entre eles é o valor inicial, o subject não precisa
+
+  productQuantity$ = this.cartSubject.pipe(map((products) => products.length));
+
+  addToCart(product: Product) {
+    const card = this.cartSubject.getValue();
+    this.cartSubject.next([...card, product]); //Adiciona na lista
+  }
 }
